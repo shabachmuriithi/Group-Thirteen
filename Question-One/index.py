4542c2840efdd782ef6358fef915b1726527dbbf
Helen Yemane <helenyemanenegash@gmail.com>
2025-05-20 21:42:50 +0300

    made some changes

---
+++ Question-One/index.py
@@ -0,0 +1,6 @@
+
+def new_event(weekly_event, event_details):
+    for daily_event in weekly_event:
+        if event_details["day"] == daily_event["day"]:
+           daily_event["event"].extend(event_details["event"])
+    return weekly_event
